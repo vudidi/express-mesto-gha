abd07f877e27cc2b504fe047b80e344d44ee532e
Vika Dovgal <vikadovgal@yandex.ru>
2022-06-02 02:31:15 +0400

    Ревью

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().regex(RegExp(regExUrl)),
+      link: Joi.string().required().regex(RegExp(regExUrl)),
     }),
   }),
   auth,
